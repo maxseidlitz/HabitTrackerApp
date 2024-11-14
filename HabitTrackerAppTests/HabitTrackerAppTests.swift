e5f20e1066dc382ae01c16da59af7412310662b9
Max Seidlitz <61022094+maxseidlitz@users.noreply.github.com>
2024-11-14 20:50:40 +0100

    Initial Commit

---
+++ HabitTrackerAppTests/HabitTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerAppTests.swift
+//  HabitTrackerAppTests
+//
+//  Created by Max Seidlitz on 14.11.24.
+//
+
+import Testing
+@testable import HabitTrackerApp
+
+struct HabitTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
